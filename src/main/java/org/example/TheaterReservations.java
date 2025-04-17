59a4a1ce40696c12b0eef6eee788482a0a1cb763
Mahmoud <maboukhelkhal@gmail.com>
2025-04-17 10:19:37 -0700

    initial commit

---
+++ src/main/java/org/example/TheaterReservations.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class TheaterReservations {
+    static Scanner input = new Scanner(System.in);
+
+
+    public String fullName;
+    public LocalDate showDate;
+    public int numberOfTickets;
+
+
+    public void getUserInfo(){
+        System.out.println("Please enter your name: ");
+        fullName = input.nextLine();
+
+        System.out.println("What date will you be coming (yyyy-mm-dd): ");
+        showDate = LocalDate.parse(input.nextLine());
+
+        System.out.println("How many tickets would you like? ");
+        numberOfTickets= input.nextInt();
+
+    }
+
+    public void displayMessage(){
+        if (numberOfTickets > 1){
+            System.out.println(numberOfTickets + " tickets reserved for " + showDate + " under " + fullName);
+        }else {
+            System.out.println(numberOfTickets + " ticket reserved for " + showDate + " under " + fullName);
+        }
+    }
+
+
+
+
+
+}
+
+
